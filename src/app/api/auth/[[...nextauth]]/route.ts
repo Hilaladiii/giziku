76b68d9c8d8e0e418c4d285e4eb6cb0ee4c63173
[abdullah hilal] <[abdulhilal242@gmail.com]>
2024-03-24 16:49:55 +0700

    feat: add next auth configuration with credentials

---
+++ src/app/api/auth/[[...nextauth]]/route.ts
@@ -0,0 +1,60 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import NextAuth from "next-auth/next";
+import { NextAuthOptions } from "next-auth";
+
+const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXT_AUTH_SECRET,
+  providers: [
+    CredentialsProvider({
+      type: "credentials",
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "email" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials) {
+        const { email, password } = credentials as {
+          email: string;
+          password: string;
+        };
+        const user: any = {
+          email: email,
+          password: password,
+        };
+
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account, user }: any) {
+      if (account.provider === "credentials") {
+        token.email = user.email;
+        (token.username = user.username), (token.role = user.role);
+      }
+      return token;
+    },
+    async session({ session, token }: any) {
+      if ("email" in token) {
+        session.user.email = token.email;
+      }
+      if ("username" in token) {
+        session.user.username = token.email;
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
