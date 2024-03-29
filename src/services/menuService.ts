22ef9075d6507ea72a043231a08a38af90e62104
Hilaladiii <abdulhilal242@gmail.com>
2024-03-29 23:13:42 +0700

    feat: add services for submit data menu

--- src/services/menuService.ts
+++ src/services/menuService.ts
@@ -1,5 +1,15 @@
+import { compositionType } from "@/types/compositionSchema";
+
 export async function getAllMenus() {
   const data = await fetch("http://localhost:3000/api/getMenu");
   const menuData = await data.json();
   return menuData;
+}
+
+export async function SubmitAllMenus(menuData: compositionType[]) {
+  const res = await fetch("http://localhost:3000/api/addMenu", {
+    method: "POST",
+    body: JSON.stringify(menuData),
+  });
+  return res.json();
 }
