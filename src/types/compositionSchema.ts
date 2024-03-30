67c4728076f7547bfc5d4f12dd99242fe28faeaa
Hilaladiii <abdulhilal242@gmail.com>
2024-03-30 15:32:55 +0700

    feat: add schema for menu

--- src/types/compositionSchema.ts
+++ src/types/compositionSchema.ts
@@ -25,4 +25,31 @@ export const compositionSchema = z.object({
   niasin: z.coerce.number().nonnegative(),
 });
 
+export const menuSchema = z.object({
+  berat: z.coerce.number().nonnegative(),
+  name: z.string().min(5, "min characters 5").max(50, "max characters 50"),
+  code: z.string().max(5, "max character 5").min(5, "min charachter 5"),
+  air: z.coerce.number().nonnegative(),
+  energi: z.coerce.number().nonnegative(),
+  protein: z.coerce.number().nonnegative(),
+  lemak: z.coerce.number().nonnegative(),
+  kh: z.coerce.number().nonnegative(),
+  serat: z.coerce.number().nonnegative(),
+  abu: z.coerce.number().nonnegative(),
+  kalsium: z.coerce.number().nonnegative(),
+  fosfor: z.coerce.number().nonnegative(),
+  besi: z.coerce.number().nonnegative(),
+  natrium: z.coerce.number().nonnegative(),
+  kalium: z.coerce.number().nonnegative(),
+  tembaga: z.coerce.number().nonnegative(),
+  seng: z.coerce.number().nonnegative(),
+  vitC: z.coerce.number().nonnegative(),
+  bKar: z.coerce.number().nonnegative(),
+  karTot: z.coerce.number().nonnegative(),
+  thiamin: z.coerce.number().nonnegative(),
+  riboflavin: z.coerce.number().nonnegative(),
+  niasin: z.coerce.number().nonnegative(),
+});
+
+export type menuType = z.infer<typeof menuSchema>;
 export type compositionType = z.infer<typeof compositionSchema>;
