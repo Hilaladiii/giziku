3fd0ed74a36cb1e05e6ef7aeacd894743f3a9c35
Hilaladiii <abdulhilal242@gmail.com>
2024-04-02 05:50:38 +0700

    feat : add service add menu by user && get menu by user

--- src/services/menuService.ts
+++ src/services/menuService.ts
@@ -1,7 +1,9 @@
-import { compositionType } from "@/types/compositionSchema";
+import { addMenuType, compositionType } from "@/types/compositionSchema";
 
 export async function getAllMenus() {
-  const data = await fetch("http://localhost:3000/api/getMenu");
+  const data = await fetch("http://localhost:3000/api/getMenu", {
+    cache: "no-store",
+  });
   const menuData = await data.json();
   return menuData;
 }
@@ -12,4 +14,28 @@ export async function SubmitAllMenus(menuData: compositionType[]) {
     body: JSON.stringify(menuData),
   });
   return res.json();
+}
+
+export async function AddMenuByUser(
+  AddMenuDataUser: addMenuType[],
+  berat: number[]
+) {
+  const data = {
+    menu: AddMenuDataUser,
+    berat: berat,
+  };
+  const res = await fetch("http://localhost:3000/api/add-menu-user", {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+  return res.json();
+}
+
+export async function getMenusByUser({ user }: { user?: string }) {
+  try {
+    const res = await fetch("http://localhost:3000/api/getMenuUser", {
+      headers: { Authorization: `${user}` },
+    });
+    return res.json();
+  } catch (error) {}
 }
