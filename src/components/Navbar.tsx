633f8d7f841094849dbee87178d6eb645c92f3dd
Hilaladiii <abdulhilal242@gmail.com>
2024-04-02 05:51:04 +0700

    feat : add navigation in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,14 +2,21 @@
 
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 export default function Navbar() {
   const { status, data } = useSession();
   return (
     <nav className="w-full h-20 bg-zinc-950 text-white flex flex-row justify-between items-center px-10 py-3">
       <h1>GIZIKU</h1>
+      <div className="flex gap-5">
+        <Link href="/dashboard">Tambah data menu</Link>
+        <Link href="/listMenu">List Menu</Link>
+        <Link href="/myMenu">Menu Pilihan Saya</Link>
+        <Link href="/result">Hasil Perhitungan</Link>
+      </div>
       <div className="flex flex-row gap-5 items-center">
-        <p>{data?.user?.name || "Guest"}</p>
+        <p>Hallo {data?.user?.name || "Guest"}</p>
         {status == "authenticated" ? (
           <Button onClick={() => signOut()}>signOut</Button>
         ) : (
