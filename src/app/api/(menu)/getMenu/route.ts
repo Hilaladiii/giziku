7c4a0f607b5d68be5db9ebf5dd8d302b460b42ea
Hilaladiii <abdulhilal242@gmail.com>
2024-04-03 18:55:30 +0700

    feat : api route for pagination get all menus

--- src/app/api/(menu)/getMenu/route.ts
+++ src/app/api/(menu)/getMenu/route.ts
@@ -2,6 +2,8 @@ import { getAllMenus } from "@/lib/DBService/dbMenu";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const res = await getAllMenus();
+  const query = req.nextUrl.searchParams.get("query") || "";
+  const currPage = Number(req.nextUrl.searchParams.get("page")) || 1;
+  const res = await getAllMenus({ query, currPage });
   return NextResponse.json(res);
 }
