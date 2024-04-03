4c012dfcdadb010d03435754be122a658823fab2
Hilaladiii <abdulhilal242@gmail.com>
2024-04-03 18:54:31 +0700

    feat : add generate pagination function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,3 +8,27 @@ export function cn(...inputs: ClassValue[]) {
 export function toFixedFloat(data: number) {
   return parseFloat(data.toFixed(3));
 }
+
+export const generatePagination = (currentPage: number, totalPages: number) => {
+  if (totalPages <= 7) {
+    return Array.from({ length: totalPages }, (_, i) => i + 1);
+  }
+
+  if (currentPage <= 3) {
+    return [1, 2, 3, "...", totalPages - 1, totalPages];
+  }
+
+  if (currentPage >= totalPages - 2) {
+    return [1, 2, 3, "...", totalPages - 2, totalPages - 1, totalPages];
+  }
+
+  return [
+    1,
+    "...",
+    currentPage - 1,
+    currentPage,
+    currentPage + 1,
+    "...",
+    totalPages,
+  ];
+};
