298304dca20d806a17913f5b4c2e182524973a9a
Hilaladiii <abdulhilal242@gmail.com>
2024-04-03 20:25:24 +0700

    refactor : add some style landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,37 @@
 import { Button } from "@/components/ui/button";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
 import Link from "next/link";
+import { authOptions } from "./api/auth/[[...nextauth]]/route";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
   return (
     <main className="w-full min-h-screen flex">
-      <div className="m-auto flex flex-col items-center">
-        <h1 className="text-3xl font-bold">Giziku for student nutrition</h1>
-        <p>
-          Giziku is platform that use to calculate the food menu for diet etc.
-        </p>
-        <Button size="lg" className="mt-3">
-          <Link href="/login">Login</Link>
-        </Button>
+      <div className="m-auto flex flex-row items-center">
+        <Image
+          src="/healty-food.gif"
+          alt="health-food picture"
+          width={400}
+          height={400}
+        />
+        <div>
+          <h1 className="text-3xl font-bold">Giziku for nutrition student</h1>
+          <p className="text-zinc-600">
+            Easy to use, Simple, Accurate results, Complete data
+          </p>
+          {session?.user ? (
+            <Button size="lg" className="mt-5">
+              <Link href="/list-menu">
+                Haii {session.user.name} let's begin
+              </Link>
+            </Button>
+          ) : (
+            <Button size="lg" className="mt-5">
+              <Link href="/login">Login</Link>
+            </Button>
+          )}
+        </div>
       </div>
     </main>
   );
