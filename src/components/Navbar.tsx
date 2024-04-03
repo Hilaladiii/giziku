167632b77dd6a8d0626281fb88b3cd5de964b0bf
Hilaladiii <abdulhilal242@gmail.com>
2024-04-03 20:25:56 +0700

    refactor : add some style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,20 +7,22 @@ import Link from "next/link";
 export default function Navbar() {
   const { status, data } = useSession();
   return (
-    <nav className="w-full h-20 bg-zinc-950 text-white flex flex-row justify-between items-center px-10 py-3">
-      <h1>GIZIKU</h1>
-      <div className="flex gap-5">
-        <Link href="/add-new-menu">Tambah data menu</Link>
+    <nav className="w-full h-20 bg-zinc-900 text-white flex flex-row justify-between items-center px-10 py-3">
+      <Link href="/" className="text-lg font-semibold">
+        Giziku
+      </Link>
+      <div className="flex gap-8">
+        <Link href="/add-new-menu">Add new data</Link>
         <Link href="/list-menu">List Menu</Link>
-        <Link href="/my-menu">Menu Pilihan Saya</Link>
-        <Link href="/result">Hasil Perhitungan</Link>
+        <Link href="/my-menu">My menu</Link>
+        <Link href="/result">Result</Link>
       </div>
       <div className="flex flex-row gap-5 items-center">
-        <p>Hallo {data?.user?.name || "Guest"}</p>
+        <p>Hello {data?.user?.name || "Guest"}👋</p>
         {status == "authenticated" ? (
-          <Button onClick={() => signOut()}>signOut</Button>
+          <Button onClick={() => signOut()}>Sign Out</Button>
         ) : (
-          <Button onClick={() => signIn()}>signIn</Button>
+          <Button onClick={() => signIn()}>Sign In</Button>
         )}
       </div>
     </nav>
