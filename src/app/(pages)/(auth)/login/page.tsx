63cefebf4bc58c299a72754025c7a146af060eb9
Hilaladiii <abdulhilal242@gmail.com>
2024-04-03 20:28:14 +0700

    refactor : add some style

--- src/app/(pages)/(auth)/login/page.tsx
+++ src/app/(pages)/(auth)/login/page.tsx
@@ -1,14 +1,24 @@
 import LoginForm from "@/components/LoginForm";
+import Link from "next/link";
 
 export default function LoginPage() {
   return (
     <main className="w-full min-h-screen flex">
       <div className="m-auto w-full max-w-md shadow-lg p-10">
         <h1 className="text-3xl font-bold">Login</h1>
-        <p className="text-sm text-zinc-400 mb-5 mt-3">
-          masukkan data anda dengan benar
+        <p className="text-sm text-zinc-500 mb-5 mt-3">
+          Please fill all data correctly
         </p>
         <LoginForm />
+        <div className="flex flex-row mt-5 justify-center items-center gap-1">
+          <p className="text-sm">Don't have an account?</p>
+          <Link
+            href="/register"
+            className="text-sm text-zinc-700 hover:text-zinc-500"
+          >
+            Register
+          </Link>
+        </div>
       </div>
     </main>
   );
--- src/app/(pages)/(auth)/register/page.tsx
+++ src/app/(pages)/(auth)/register/page.tsx
@@ -1,4 +1,5 @@
 import RegisterForm from "@/components/RegisterForm";
+import Link from "next/link";
 
 export default function LoginPage() {
   return (
@@ -6,9 +7,18 @@ export default function LoginPage() {
       <div className="m-auto w-full max-w-md shadow-lg p-10">
         <h1 className="text-3xl font-bold">Register</h1>
         <p className="text-sm text-zinc-400 mb-5 mt-3">
-          masukkan data anda dengan benar
+          Please fill all data correctly
         </p>
         <RegisterForm />
+        <div className="flex flex-row mt-5 justify-center items-center gap-1">
+          <p className="text-sm">Already have an account?</p>
+          <Link
+            href="/login"
+            className="text-sm text-zinc-700 hover:text-zinc-500"
+          >
+            Login
+          </Link>
+        </div>
       </div>
     </main>
   );
