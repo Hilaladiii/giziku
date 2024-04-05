202ee2004818c86858a2d3ca7b0389c9f83ccac9
Hilaladiii <abdulhilal242@gmail.com>
2024-04-06 03:31:37 +0700

    refactor : change type of action reducer && type of store context

--- src/contexts/MenuContext.tsx
+++ src/contexts/MenuContext.tsx
@@ -1,14 +1,14 @@
-import { MenuType } from "@/types/menuSchema";
+import { NewMenuType } from "@/types/menuSchema";
 import { useReducer, useContext, createContext, Dispatch } from "react";
 
 type ActionMenuType = {
   type: string;
-  payload: MenuType;
+  payload: NewMenuType;
 };
-const menuContext = createContext<MenuType[]>([]);
+const menuContext = createContext<NewMenuType[]>([]);
 const menuDispatch = createContext<Dispatch<ActionMenuType>>(() => {});
 
-const menuReducer = (state: MenuType[], action: ActionMenuType) => {
+const menuReducer = (state: NewMenuType[], action: ActionMenuType) => {
   switch (action.type) {
     case "ADD_MENU":
       return [...state, action.payload];
