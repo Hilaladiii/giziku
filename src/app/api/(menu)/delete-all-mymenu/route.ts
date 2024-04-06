f9ea089553009c97434cf8eae56d67406c884bcf
Hilaladiii <abdulhilal242@gmail.com>
2024-04-06 22:57:00 +0700

    refactor : delete console.log

--- src/app/api/(menu)/delete-all-mymenu/route.ts
+++ src/app/api/(menu)/delete-all-mymenu/route.ts
@@ -3,8 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function DELETE(req: NextRequest) {
   const user = req.headers.get("Authorization") || "";
-  console.log(user);
-
   const res = await deleteAllMyMenu({ user });
   return NextResponse.json(res);
 }
