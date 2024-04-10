a3cf77e5bf780ff41b6a4d5c96605451fc17c0c6
Hilaladiii <abdulhilal242@gmail.com>
2024-04-10 18:42:21 +0700

    refactor : delete unused code

--- src/app/(pages)/(auth)/login/page.tsx
+++ src/app/(pages)/(auth)/login/page.tsx
@@ -1,23 +1,22 @@
+'use client'
+
 import LoginForm from "@/components/LoginForm";
 import Link from "next/link";
-import { Suspense } from "react";
 
 export default function LoginPage() {
   return (
     <main className="w-full min-h-screen flex">
-      <div className="m-auto w-full max-w-md shadow-lg p-10">
+      <div className="m-auto w-full max-w-md shadow-md p-10">
         <h1 className="text-3xl font-bold">Login</h1>
         <p className="text-sm text-zinc-500 mb-5 mt-3">
           Please fill all data correctly
         </p>
-        <Suspense fallback={<h1>Loading..</h1>}>
-          <LoginForm />
-        </Suspense>
+        <LoginForm />
         <div className="flex flex-row mt-5 justify-center items-center gap-1">
           <p className="text-sm">Dont have an account?</p>
           <Link
             href="/register"
-            className="text-sm text-zinc-700 hover:text-zinc-500"
+            className="text-sm text-zinc-700 hover:text-zinc-500 underline"
           >
             Register
           </Link>
