6466a95856899c60ca121b72c6ff76922c02c784
Hilaladiii <abdulhilal242@gmail.com>
2024-04-18 06:14:35 +0700

    feat : add loading calculating result

---
+++ src/app/(pages)/result/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="w-full min-h-screen flex justify-center items-center">
+      <h1 className="font-semibold text-3xl">Calculating your result...</h1>
+    </div>
+  );
+}
